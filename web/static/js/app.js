a7d2c41789626464a8630182d4d44aa683aa4857
Keith Salisbury <keithsalisbury@gmail.com>
2016-05-05 23:07:59 +0100

    new game instance in app.js

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -8,14 +8,7 @@
 // therefore are always executed.
 
 // Import dependencies
-//
-// If you no longer want to use a dependency, remember
-// to also remove its path from "config.paths.watched".
-import "phoenix_html"
+import {Game} from "./Game"
 
-// Import local files
-//
-// Local files can be imported directly using relative
-// paths "./socket" or full ones "web/static/js/socket".
-
-// import socket from "./socket"
+// Lets go!
+new Game(700, 450, "phaser")
