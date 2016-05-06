e687ed28d233d37c523d9bc3ca5bfd40d238ae07
Keith Salisbury <keithsalisbury@gmail.com>
2016-05-06 14:48:13 +0100

    receive and log the message

--- web/static/js/common/sync.js
+++ web/static/js/common/sync.js
@@ -1,9 +1,10 @@
 // serializePosition :: Sprite -> Object
 export const serializePosition = ({x, y}) => Object.assign({x, y})
 
-// syncPosition :: Sprite -> Channel -> Event -> Function -> Event -> Event
+// syncPosition :: Sprite -> Channel -> Event -> Function -> Event
 export const syncPosition = (sprite, channel, event) => {
   event.add(sprite => sendPosition(sprite, channel))
+  receivePosition(sprite, channel)
 }
 
 // sendPosition :: Sprite -> Channel -> Push
@@ -11,4 +12,12 @@ export const sendPosition = (sprite, channel) => {
   const message = serializePosition(sprite)
   console.log("Sending message", message)
   channel.push("shout", message)
+}
+
+// receivePosition = Sprite -> Channel -> Push
+export const receivePosition = (sprite, channel) => {
+  const callback = (message) => {
+    console.log("Received message", message)
+  }
+  channel.on("shout", callback)
 }
