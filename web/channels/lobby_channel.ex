826bc95f54fe24a2f6122308a0d42ea78a5a0ae2
Keith Salisbury <keithsalisbury@gmail.com>
2016-05-06 03:02:44 +0100

    mix gen channel

---
+++ web/channels/lobby_channel.ex
@@ -0,0 +1,37 @@
+defmodule Demo.LobbyChannel do
+  use Demo.Web, :channel
+
+  def join("games:lobby", payload, socket) do
+    if authorized?(payload) do
+      {:ok, socket}
+    else
+      {:error, %{reason: "unauthorized"}}
+    end
+  end
+
+  # Channels can be used in a request/response fashion
+  # by sending replies to requests from the client
+  def handle_in("ping", payload, socket) do
+    {:reply, {:ok, payload}, socket}
+  end
+
+  # It is also common to receive messages from the client and
+  # broadcast to everyone in the current topic (games:lobby).
+  def handle_in("shout", payload, socket) do
+    broadcast socket, "shout", payload
+    {:noreply, socket}
+  end
+
+  # This is invoked every time a notification is being broadcast
+  # to the client. The default implementation is just to push it
+  # downstream but one could filter or change the event.
+  def handle_out(event, payload, socket) do
+    push socket, event, payload
+    {:noreply, socket}
+  end
+
+  # Add authorization logic here as required.
+  defp authorized?(_payload) do
+    true
+  end
+end
