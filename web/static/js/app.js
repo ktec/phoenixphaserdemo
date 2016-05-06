2f780f75b3edf6cd14c6fb689fd2bca277201e0f
Keith Salisbury <keithsalisbury@gmail.com>
2016-05-06 03:36:30 +0100

    import and pass the socket to game

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -9,6 +9,10 @@
 
 // Import dependencies
 import {Game} from "./Game"
+import {Socket} from "phoenix"
+
+const socket = new Socket("/socket", {})
+const game = new Game(700, 450, "phaser")
 
 // Lets go!
-new Game(700, 450, "phaser")
+game.start(socket)
